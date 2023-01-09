842cb47533184d5002f5bc750c79e53a149e070f
unknown <y2h000123>
2023-01-10 03:16:44 +0900

    gui

--- kafl_gui.py
+++ kafl_gui.py
@@ -291,8 +291,8 @@ class GuiDrawer:
             if 'x_now' in d.stats['pso']:
                 self.gui.print_thin_line()
                 for i in range(len(d.stats['pso']['x_now'])-1):
-                    self.stdscr.addstr(self.y, 0, 'swarm%2d: '%i + " ".join(d.pso_x_now(i)))
-                    self.y += 1
+                    self.gui.stdscr.addstr(self.gui.y, 0, 'swarm%2d: '%i + " ".join(d.pso_x_now(i)))
+                    self.gui.y += 1
 
         i = self.current_slave_id
         self.gui.print_thin_line()
