aa864bfba4dfc80b7585e4eb7a8db9ef473bd528
unknown <y2h000123>
2023-01-12 15:07:10 +0900

    patch bash code

--- kafl_gui.py
+++ kafl_gui.py
@@ -289,8 +289,8 @@ class GuiDrawer:
                                       (30, 'progress', d.pso_progress()),
                                       (13, 'cycles', '%4d'%d.pso_cycles())], prefix="PSO: ")
         
-        self.gui.print_thin_line()
-        self.gui.print_info_line([(78, 'Pacemaker', d.pacemaker_state())])
+        #self.gui.print_thin_line()
+        #self.gui.print_info_line([(78, 'Pacemaker', d.pacemaker_state())])
 
 
         i = self.current_slave_id
